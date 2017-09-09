8cef64f4e298e86a24aa55a5688cb507749dbede
Miguel Jardón <migueljp83@gmail.com>
2017-09-09 11:40:54 +0200

    Commit inicial.

---
+++ MadridShops/Model/Shop.swift
@@ -0,0 +1,19 @@
+//
+//  Shop.swift
+//  MadridShops
+//
+//  Created by MIGUEL JARDÓN PEINADO on 9/9/17.
+//  Copyright © 2017 Ammyt. All rights reserved.
+//
+
+import Foundation
+
+class Shop {
+    let name: String
+    let description: String
+    let latitude: Float
+    let longitude: Float
+    let image: String
+    let logo: String
+    let openingHours: String
+}
